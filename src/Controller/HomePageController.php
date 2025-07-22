2533d7a8bac0537898e888643e4606ef0ebab28e
Patience-Kori1 <koribiram@yahoo.fr>
2025-07-22 08:42:14 +0200

    Implement Create logic of CRUD feature: - Instantiate Crud entity and create form using CrudType - Handle form submission with isSubmitted() and isValid() - Save data to database using persist() and flush() - Add flash message on successful submission - Redirect user to home page after validation - Retrieve data from CrudRepository using findAll() - Display data dynamically in a Bootstrap-styled HTML table on the home page

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Crud;
 use App\Form\CrudTpeType;
+use App\Repository\CrudRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,22 +14,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 final class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function homePage(): Response
+    public function homePage(CrudRepository $repository ): Response
     {
+        $datas = $repository->findAll();
         return $this->render('home_page/homePage.html.twig', [
-            'controller_name' => 'HomePageController',
+            'datas'=> $datas,
         ]);
     }
 
-    #[Route('/create', name: 'app_create_form')]
-    public function create_form(Request $request): Response
+    #[Route('/create', name: 'app_create')]
+    public function create(Request $request , EntityManagerInterface $em): Response
     {
         $crud = new Crud();
         $form = $this -> createForm(CrudTpeType::class, $crud);
         $form->handleRequest($request);
-    
+            if($form->isSubmitted() && $form->isValid()) {
+                $em->persist($crud);
+                $em ->flush();
+
+                $this->addFlash('success', 'Données enregistrées avec succès !');
+                return $this->redirectToRoute('app_home_page');
+            }
         return $this->render('form/createForm.html.twig', [
             'form' => $form ->createView(),
-        ]);
+
+        ]);  
     }
 }
